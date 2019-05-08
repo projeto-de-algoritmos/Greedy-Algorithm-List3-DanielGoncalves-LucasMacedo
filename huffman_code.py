110771ca6478c2fe78319c5c5151440b68070914
DanMke <danmke@hotmail.com>
2019-05-08 19:27:06 -0300

    Implemented fill heap and join nodes to make huffman tree
    Signed-off-by: DanMke <danmke@hotmail.com>

--- huffman_code.py
+++ huffman_code.py
@@ -6,7 +6,7 @@ class Node:
         self.frequency = frequency
         self.right = None
         self.left = None
-
+    # para a heap não dar erro no heappush quando há frequencias iguais
     def __lt__(self, other):
         return self.frequency < other.frequency
 
@@ -17,9 +17,13 @@ class Huffman:
         self.characters_frequency = {}
         self.characters_codes = {}
         self.heap = []
+        self.compressed_text = ''
 
-        self.get_character_frequency()
-        self.fill_heap()
+        for character in self.text:
+            if not character in self.characters_codes:
+                self.characters_codes[character] = ''
+
+        self.compress_text()
 
     def get_character_frequency(self):
 
@@ -32,6 +36,20 @@ class Huffman:
 
         for key in self.characters_frequency:
             heapq.heappush(self.heap, Node(key, self.characters_frequency[key]))
+
+    def join_nodes(self):
+        # juntar os nos da heap ate que tenha so um no com os filhos
+        # ele pega as duas menores frequencias da heap e junta ate que tenha so 1
+        while(len(self.heap) > 1):
+            node_one = heapq.heappop(self.heap)
+            node_two = heapq.heappop(self.heap)
+            print("ESTE EH O 1: " + str(node_one.character) + " " + str(node_one.frequency))
+            print("ESTE EH O 2: " + str(node_two.character) + " " + str(node_two.frequency))
+            join_node = Node(None, node_one.frequency + node_two.frequency)
+            join_node.left = node_one
+            join_node.right = node_two
+
+            heapq.heappush(self.heap, join_node)
 
 
 def main():
@@ -48,6 +66,8 @@ def main():
 
     for element in huffman.heap:
         print(str(element.character) + ' : ' + str(element.frequency))
+
+    print(huffman.characters_codes)
 
 
 if __name__ == '__main__':
